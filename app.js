8578645a060c4ee383db26b798c9370ce7db4901
tomasbarak <barakcinthia@gmail.com>
2021-11-22 19:51:56 -0300

    Added create user end point

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
+var bodyParser = require('body-parser');
+
 const app = express()
 var mysql      = require('mysql');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 var connection = mysql.createConnection({
-  host     : 'sql10.freemysqlhosting.net',
-  user     : 'sql10452023',
-  password : 'pVRwvnxw5D',
-  database : 'sql10452023'
+  host     : process.env.db_host,
+  user     : process.env.user,
+  password : process.env.password,
+  database : process.env.name
 });
 connection.connect();
 
@@ -20,6 +25,19 @@ app.get('/users/:name/all/', function (req, res) {
         res.send(results)
       });
   console.log(req.params.name)
+})
+app.post('/create/user/', function (req, res){
+  connection.query('INSERT INTO users (name, mail, age, phone) VALUES ('+ '"' + req.body.name + '", ' + '"' + req.body.mail + '", ' + '"' + req.body.age + '", ' + '"' + req.body.phone + '")' , function(error, results, fields){
+    console.log(req.body);
+    if(error){
+      res.status(400)
+      res.send(error)
+    }else{
+      res.status(200)
+      res.send(results)
+    }
+  })
+  
 })
  
 app.listen(3000)
