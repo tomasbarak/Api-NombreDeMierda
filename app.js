9a0823c100421435f36e5b4e7dd0076f5e7ab179
tomasbarak <barakcinthia@gmail.com>
2021-11-22 20:03:43 -0300

    Added env vars compatibility

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express')
 var bodyParser = require('body-parser');
 
+require('dotenv').config()
+
 const app = express()
 var mysql      = require('mysql');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host     : process.env.db_host,
-  user     : process.env.user,
-  password : process.env.password,
-  database : process.env.name
+  user     : process.env.db_user,
+  password : process.env.db_password,
+  database : process.env.db_name
 });
 connection.connect();
 
 app.get('/users/:name/all/', function (req, res) {
+  //Query a la base de datos para obtener un usuario
     connection.query('SELECT * from users where name = "' + req.params.name +'"', function (error, results, fields) {
         if (error){
+          //Ocurrio un error
             res.status(400)
             res.send(error)
         } 
@@ -26,15 +30,21 @@ app.get('/users/:name/all/', function (req, res) {
       });
   console.log(req.params.name)
 })
+
 app.post('/create/user/', function (req, res){
+  //Query a la base de datos para crear un usuario
   connection.query('INSERT INTO users (name, mail, age, phone) VALUES ('+ '"' + req.body.name + '", ' + '"' + req.body.mail + '", ' + '"' + req.body.age + '", ' + '"' + req.body.phone + '")' , function(error, results, fields){
     console.log(req.body);
     if(error){
+      //Ocurrio un error
       res.status(400)
       res.send(error)
+
     }else{
+      //Los datos se insertaron correctamente
       res.status(200)
       res.send(results)
+
     }
   })
   
