e25759e3bbd5c30b11fd696b6b2dc1d082514f3a
tomasbarak <alumno23.barak.tomas@ipm.edu.ar>
2021-11-23 08:20:16 -0300

    Added id from user endpoint

--- app.js
+++ app.js
@@ -34,6 +34,21 @@ app.get('/users/:name/all/', function (req, res) {
       });
   console.log(req.params.name)
 })
+app.get('/users/:name/id/', function (req, res) {
+  //Query a la base de datos para obtener un usuario
+    connection.query('SELECT id from users where name = "' + req.params.name +'"', function (error, results, fields) {
+        if (error){
+          //Ocurrio un error
+            res.status(400)
+            res.send(error)
+        }else{
+          //Se obtuvieron los datos correctamente y se devuelven
+          res.status(200)
+          res.send(results)
+        }
+      });
+  console.log(req.params.name)
+})
 
 app.post('/create/user/', function (req, res){
   //Query a la base de datos para crear un usuario
